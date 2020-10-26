ddc13d318aee417beb113395c3ffb2d56e1931d5
Ariel I <arielibarra39@gmail.com>
2020-10-26 20:07:22 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+<<<<<<< HEAD
   get '/articles' do
     @articles = Article.all
     erb :index
@@ -47,5 +48,35 @@ class ApplicationController < Sinatra::Base
     
     redirect to '/articles'
   end
+=======
+  get '/articles' do 
+    @articles = Article.all
+    erb :index
+  end 
+  #good here
+  
+  get 'articles/new' do 
+    erb :new 
+  end
+
+ post '/articles' do
+    @article = Article.create(params)
+
+    redirect to "/articles/id"
+  end
+
+  get '/articles/:id' do
+    @article = Article.find_by_id(params[:id])
+
+    erb :show
+  end
+  
+  
+ 
+  
+  
+  
+  
+>>>>>>> 4e87afc3b79eb26d0ba6cfff3b3fb094a850ab21
   
 end
